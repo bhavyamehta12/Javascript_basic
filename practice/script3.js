9fe44ba07becca68090fcd453e641465081651ec
bhavyamehta12 <bhavya.mehta@sahanasystems.com>
2026-09-16 17:13:11 +0530

    Create script3.js

---
+++ practice/script3.js
@@ -0,0 +1,17 @@
+// import {count,increment} from './script4.js'
+
+// console.log(count)
+// increment()
+// console.log(count)
+// // let count = 90;
+// // console.log(count)
+// // count = 90
+// import name from './script4.js'
+// console.log(name)
+// import {name} from './script4.js'
+// console.log(name)
+// import {login} from './script4.js'
+// console.log(login())
+import * as script4 from './script4.js'
+console.log(script4.name)
+console.log(script4.default())
