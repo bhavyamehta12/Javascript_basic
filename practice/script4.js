79725ce78b297123f90f78fcef0b939a41df9bbe
bhavyamehta12 <bhavya.mehta@sahanasystems.com>
2026-09-16 17:13:15 +0530

    Create script4.js

---
+++ practice/script4.js
@@ -0,0 +1,17 @@
+// let count = 0
+// function increment(){
+//     count++;
+// }
+// export {increment,count} 
+// // export default const name = "Bhavya";it wont work
+// const name = "bhavya";
+// export default name;
+// export const name = "Bhavya";   
+// export default function login(){
+//     console.log("Login successful")
+// }
+export const name = "Bhavya";
+export default function login() {
+    console.log("Login successful");
+}
+export * from './script4.js';   
